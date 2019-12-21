4529672a9298d75dce26ba037f9c1d205592d35b
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-21 20:42:59 +0100

    added velocity instance variables to update method to bounce ruby around window

--- code/01-whack_a_ruby/whack_a_ruby.rb
+++ code/01-whack_a_ruby/whack_a_ruby.rb
@@ -9,8 +9,8 @@ class WhackARuby < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
-    @velocity_x = 5
-    @velocity_y = 5
+    @velocity_x = 1
+    @velocity_y = 1
   end
 
   def draw
@@ -20,6 +20,8 @@ class WhackARuby < Gosu::Window
   def update
     @x += @velocity_x
     @y += @velocity_y
+    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @width / 2 > 600 || @y - @width / 2 < 0
   end
 end
 
