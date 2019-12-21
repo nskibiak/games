8b0dc875a2fd244c307cd353b89323499ac60aee
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-21 20:24:36 +0100

    added images to folder

--- code/01-whack_a_ruby/whack_a_ruby.rb
+++ code/01-whack_a_ruby/whack_a_ruby.rb
@@ -4,6 +4,9 @@ class WhackARuby < Gosu::Window
   def initialize
     super(800, 600)
     self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
   end
 end
 
