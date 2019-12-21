8b7eed63cd1c2e41c9fc8bdd62afb866bccbccfb
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-21 20:27:12 +0100

    added draw method that positions ruby png

--- code/01-whack_a_ruby/whack_a_ruby.rb
+++ code/01-whack_a_ruby/whack_a_ruby.rb
@@ -7,6 +7,12 @@ class WhackARuby < Gosu::Window
     @image = Gosu::Image.new('ruby.png')
     @x = 200
     @y = 200
+    @width = 50
+    @height = 43
+  end
+
+  def draw
+    @image.draw(@x - @width / 2, @y - @height / 2, 1)
   end
 end
 
